7f0d68a433372a9313fcc2ce63ba7d5c7d2299a0
WladKarasew <karasyow600@gmail.com>
2018-04-07 14:48:01 +0300

    commit during lesson

--- src/by/it/malets/lesson02/TaskA2.java
+++ src/by/it/malets/lesson02/TaskA2.java
@@ -1,5 +1,7 @@
 package by.it.malets.lesson02;
 
+import java.io.IOException;
+
 /*
 напишите тело main программы которая выводит на экран 5 строк
 "Я начинаю изучать Java!"
@@ -8,5 +10,9 @@ package by.it.malets.lesson02;
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
+    public static void main(String[] args) throws IOException
+            String s=" System.out.println(Я начинаю изучать Java;
+                    
+
 
 }
